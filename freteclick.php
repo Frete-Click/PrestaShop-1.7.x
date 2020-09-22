ecae047572a5778565fb0c5a9060ab5bd42f49f9
Luiz Kim <luiz.kim@controleonline.com>
2020-09-21 23:43:34 -0300

    Add SDK

--- freteclick.php
+++ freteclick.php
@@ -1,4 +1,5 @@
 <?php
+require_once (dirname(__FILE__).'/vendor/autoload.php');
 
 use PrestaShop\PrestaShop\Core\Addon\Module\ModuleManager;
 use PrestaShop\PrestaShop\Core\Addon\Module\ModuleManagerBuilder;
